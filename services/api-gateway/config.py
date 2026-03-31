b64977c0e8ebf042857c6e50f861b0f984d8a4e3
Juvan Paulo <juvanpaulo1@gmail.com>
2026-03-31 14:15:28 +0800

    feat: update docker-compose and config for API gateway service routing

--- services/api-gateway/config.py
+++ services/api-gateway/config.py
@@ -2,10 +2,12 @@ import os
 
 
 IDENTITY_SERVICE_URL = os.getenv("IDENTITY_SERVICE_URL", "http://localhost:8010")
-MATH_ADD_SERVICE_URL = os.getenv("MATH_ADD_SERVICE_URL", "http://localhost:8011")
-MATH_SUBTRACT_SERVICE_URL = os.getenv("MATH_SUBTRACT_SERVICE_URL", "http://localhost:8012")
-MATH_MULTIPLY_SERVICE_URL = os.getenv("MATH_MULTIPLY_SERVICE_URL", "http://localhost:8013")
-MATH_DIVIDE_SERVICE_URL = os.getenv("MATH_DIVIDE_SERVICE_URL", "http://localhost:8014")
+
+# For docker compose, route to the service name within the docker network instead of localhost
+MATH_ADD_SERVICE_URL = os.getenv("MATH_ADD_SERVICE_URL", "http://math-add:8000")
+MATH_SUBTRACT_SERVICE_URL = os.getenv("MATH_SUBTRACT_SERVICE_URL", "http://math-subtract:8000")
+MATH_MULTIPLY_SERVICE_URL = os.getenv("MATH_MULTIPLY_SERVICE_URL", "http://math-multiply:8000")
+MATH_DIVIDE_SERVICE_URL = os.getenv("MATH_DIVIDE_SERVICE_URL", "http://math-divide:8000")
 
 RATE_LIMIT_REQUESTS = int(os.getenv("RATE_LIMIT_REQUESTS", "60"))
 RATE_LIMIT_WINDOW_SECONDS = int(os.getenv("RATE_LIMIT_WINDOW_SECONDS", "60"))
